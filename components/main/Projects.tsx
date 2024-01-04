60037e765596a8668b44ce27b8c20b5a49f243dc
ishtiaktauhid <tauhidulislam1516@gmail.com>
2024-01-04 12:03:27 +0600

    Git

--- components/main/Projects.tsx
+++ components/main/Projects.tsx
@@ -4,7 +4,10 @@ import ProjectCard from "../sub/ProjectCard";
 const Projects = () => {
   return (
     <div className="flex flex-col items-center justify-center py-20">
-      <h1 className="text-[40px] font-semibold text-transparent bg-clip-text bg-gradient-to-r from-purple-500 to-cyan-500 py-20" id="projects">
+      <h1
+        className="text-[40px] font-semibold text-transparent bg-clip-text bg-gradient-to-r from-purple-500 to-cyan-500 py-20"
+        id="projects"
+      >
         My Projects
       </h1>
       <div className="h-full w-full flex flex-col md:flex-row gap-10 px-10">
--- components/sub/HeroContent.tsx
+++ components/sub/HeroContent.tsx
@@ -32,12 +32,12 @@ const HeroContent = () => {
           className="flex flex-col gap-6 mt-6 text-6xl font-bold text-white max-w-[600px] w-auto h-auto "
         >
           <span>
-            Providing
+            HI! I'm
             <span className="text-transparent bg-clip-text bg-gradient-to-r from-purple-500 to-cyan-500">
               {" "}
-              the best{" "}
+              Asadullah Al Galib{" "}
             </span>
-            project experience
+            A Full Stack Developer
           </span>
         </motion.div>
         <motion.p
