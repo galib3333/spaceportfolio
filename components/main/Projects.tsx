10e8ac48af60ad59350e6cbe1f8354ba56fd8eb6
galib333 <mohapurush6@gmail.com>
2024-01-05 01:46:52 +0600

    buf fix

--- components/main/Projects.tsx
+++ components/main/Projects.tsx
@@ -15,19 +15,16 @@ const Projects = () => {
             src="/themedev.png"
             title="Modern WordPress Theme Development"
             description="Lorem ipsum dolor sit amet consectetur adipisicing elit. Exercitationem rerum vitae sint quibusdam delectus tenetur!"
-            link="https://wdpf54.tech/theme_dev"
           />
           <ProjectCard
             src="/reactweb.png"
             title="Interactive React JS Website"
             description="Lorem ipsum dolor sit amet consectetur adipisicing elit. Exercitationem rerum vitae sint quibusdam delectus tenetur!"
-            link="https://example.com"
           />
           <ProjectCard
             src="/laravel.png"
             title="Laravel Hospital Management System"
             description="Lorem ipsum dolor sit amet consectetur adipisicing elit. Exercitationem rerum vitae sint quibusdam delectus tenetur!"
-            link="https://example.com"
           />
       </div>
     </div>
